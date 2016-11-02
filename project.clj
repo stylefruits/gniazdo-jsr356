aca505b9b254fc1d5811a881c4aca86e755b0200
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-02 11:14:04 +0100

    stay in line with gniazdo's versioning.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stylefruits/gniazdo "2.0.1-SNAPSHOT"
+(defproject stylefruits/gniazdo "1.0.0-SNAPSHOT"
   :description "A WebSocket client for Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.glassfish.tyrus/tyrus-client "1.9"]
